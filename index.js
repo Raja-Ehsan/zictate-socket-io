6e333b8c458de4fbd56beaa3af07abcabcc29e06
rajaehsan.0022@gmail.com <91175440+rajaehsan1234@users.noreply.github.com>
2022-08-04 09:20:36 -0700

    make it better

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+var app = express();
+const io= require('socket.io')(8000,{
+    cors:{
+        origin:"https://master--deluxe-sable-a44c90.netlify.app",
+    },
+})
+
+const PORT=process.env.PORT || 3002;
+
+app.get('/',(req,res)=>{
+    res.write(`SOcket started on ${PORT}`);
+    res.send();
+})
+let users=[];
+
+const addUser =(userId,user_name,profileImage,socketId)=>{
+    if(userId){
+    !users.some(user=>user.userId===userId) && 
+    users.push({userId,user_name,profileImage,socketId});}
+}
+
+const removeUser=(socketId)=>{
+    users=users.filter(user=>user.socketId!==socketId)
+}
+
+const getUser=(userId)=>{
+    return users.find(user=>user.userId===userId)
+}
+
+io.on('connection', socket=>{
+    console.log("New User COnnected.")
+    io.emit("welcome","hello this is socket server!");
+
+    socket.on("addUser",(userId,user_name,profileImage)=>{
+        addUser(userId,user_name,profileImage,socket.id);
+        console.log(users)
+         io.emit("getUsers",users);
+    })
+
+    socket.on("sendMessage",({senderId,receiverId,text})=>{
+        console.log(senderId,receiverId,text)
+        const user=getUser(receiverId)
+        console.log(users)
+        user?io.to(user.socketId).emit("getMessage",{
+            senderId,
+            text,
+        }):""
+    })
+
+    socket.on("disconnect",()=>{
+        console.log("a user disconnected");
+        removeUser(socket.id)
+        io.emit("getUsers",users)
+    }) 
+} )
+
+app.listen( PORT, () => {
+    console.log("Server started on port 3001")
+})
